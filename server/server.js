7ef61389b666f3fa6244ae9247932d626d49cdd5
changhc <changhc84@gmail.com>
2017-06-15 15:48:58 +0800

    remove legacy code

--- server/server.js
+++ server/server.js
@@ -50,60 +50,6 @@ server.post('/api/case', (req, res) => {
   res.send(200, JSON.stringify(body));
 });
 
-server.post('/api/register', (req, res) => {
-  const ip = req.connection.remoteAddress.replace('::ffff:', '');
-  const deviceId = req.body.deviceId;
-  if (!ipList[deviceId] || ipList[deviceId] !== ip) {
-    console.log(`new device: ${deviceId}`);
-    ipList[deviceId] = { ip: ip, coordN: req.body.coordN, coordE: req.body.coordE };
-  }
-  res.send(200);
-});
-
-server.post('/api/device', (req, res) => {
-  const deviceId = req.body.deviceId;
-  if (!ipList.hasOwnProperty(deviceId)) {
-    res.send(403, 'no registration');
-  }
-  try {
-    if (parseInt(deviceId, 10) < 0) {
-      throw Error('bad request');
-    }
-    if (parseFloat(deviceId) !== parseInt(deviceId, 10)) {
-      throw Error('bad request');
-    }
-    if (!database[deviceId]) {
-      database[deviceId] = [];
-    }
-    database[deviceId].push({
-      timestamp: req.body.timestamp,
-      temp: req.body.temp,
-      humidity: req.body.humidity,
-      servoSpeed: req.body.servoSpeed,
-    });
-    res.send(202);
-  } catch (err) {
-    console.log(err);
-    res.send(400);
-  }
-});
-
-server.put('/api/control', (req, res) => {
-  try{
-    const key = req.body.deviceId;
-    if (ipList.hasOwnProperty(key)) {
-      console.log(ipList[key]);
-      request.put({
-        headers: { 'content-type': 'application/json' },
-        url: `http://${ipList[key].ip}:9999/command`,
-        body: JSON.stringify(req.body),
-      }, (err, res2, body) => {
-        if (err) console.log(err);
-      });
-    }
-    res.send(200);
-  } catch (err) { console.log(err); res.send(500, err); }
-});
 
 server.get(/\/*/, restify.serveStatic({
   directory: path.join(__dirname, '/public'),
